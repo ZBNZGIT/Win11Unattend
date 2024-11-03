d782b0bc9d0b93832e0a9767d3dd2a67f72ceee6
ZBNZ <68793343+ZBNZGIT@users.noreply.github.com>
2024-11-03 19:23:23 +1300

    Add files via upload

---
+++ ScriptsLauncher.ps1
@@ -0,0 +1,38 @@
+$script1Url = "https://raw.githubusercontent.com/ZBNZGIT/AppsInstaller/main/ChocolateyInstallApps.bat"
+$script2Url = "https://raw.githubusercontent.com/ZBNZGIT/RemoveEdge/main/RemovesEdge.bat"
+
+function Show-Menu {
+    Clear-Host
+    Write-Host "1. Install Apps"
+    Write-Host "2. Remove Edge" 
+    Write-Host "3. Activate Windows (It does take a bit to open)"
+    Write-Host "4. Exit"
+}
+
+function Run-Script {
+    param ([string]$scriptUrl)
+    try {
+        $scriptContent = Invoke-RestMethod -Uri $scriptUrl
+        $tempFile = [System.IO.Path]::GetTempFileName() + ".bat"
+        Set-Content -Path $tempFile -Value $scriptContent
+        Start-Process -FilePath "cmd.exe" -ArgumentList "/c $tempFile" -WindowStyle Normal
+    }
+    catch {
+        Write-Error "An error occurred: $_"
+    }
+}
+
+do {
+    Show-Menu
+    switch (Read-Host "Please select an option (1-3)") {
+        '1' { Run-Script -scriptUrl $script1Url; exit }
+        '2' { Run-Script -scriptUrl $script2Url; exit }
+        '3' { Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://get.activated.win | iex`"" -WindowStyle Hidden; Stop-Process -Id $PID }
+        '4' { exit }
+        default { Write-Host "Invalid choice, please try again." }
+    }
+} while ($true)
+
+
+timeout -t 3 -nobreak > nul
+
